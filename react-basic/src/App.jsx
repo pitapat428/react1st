19ecc22aca4873402ebbc71f12e7a0a071d9a8bf
delim <tdixxgidt@gmail.com>
2024-05-28 16:58:06 +0900

    리액트 기본 개념 & useState 익숙해지기

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  const addCount = () => {
+    setCount(count + 1);
+  };
+
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  const changeMode = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <>
+      <div className={isDark ? 'dark-mode' : 'light-mode'}>
+        {isDark ? '다크모드' : '화이트모드'}
+      </div>
+      <div className="container">
+        <button className="change-mode" onClick={changeMode}>
+          Change Mode
+        </button>
+        <div>Count: {count}</div>
+        <button className="add" onClick={addCount}>
+          +
+        </button>
+        <button className="minus" onClick={minusCount}>
+          -
+        </button>
+        <button className="reset" onClick={resetCount}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
